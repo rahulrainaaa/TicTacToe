3b43968a6631fa49c0d5f905906a4ae17759d992
rahulrainahomeee <rahulrainahome@gmail.com>
2017-03-07 12:49:14 +0530

    initial commit

--- app/src/main/java/app/project/tictactoe/activity/SelectActivity.java
+++ app/src/main/java/app/project/tictactoe/activity/SelectActivity.java
@@ -2,21 +2,51 @@ package app.project.tictactoe.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import app.project.tictactoe.R;
 
-public class SelectActivity extends AppCompatActivity implements View.OnClickListener {
+public class SelectActivity extends FragmentActivity implements View.OnClickListener {
+
+    private Animation animationM = null;
+    private Animation animationQ = null;
+
+    private ImageView imgM = null;
+    private ImageView imgQ = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
 
-        findViewById(R.id.btn_qr).setOnClickListener(this);
-        findViewById(R.id.btn_number).setOnClickListener(this);
+        animationM = AnimationUtils.loadAnimation(this, R.anim.cloudm);
+        animationQ = AnimationUtils.loadAnimation(this, R.anim.cloudq);
+
+        imgQ = (ImageView) findViewById(R.id.btn_qr);
+        imgM = (ImageView) findViewById(R.id.btn_number);
+
+        imgQ.setOnClickListener(this);
+        imgM.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        imgM.startAnimation(animationM);
+        imgQ.startAnimation(animationQ);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        imgM.clearAnimation();
+        imgQ.clearAnimation();
     }
 
     @Override
